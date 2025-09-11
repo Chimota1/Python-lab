7f68acd940a7f8ddfa04687009bd95493dc86a80
Anatoliy <avkindrat2006@gmail.com>
2025-09-11 19:45:48 +0300

    End of project

--- lab1.1.py
+++ lab1.1.py
@@ -1,8 +1,26 @@
 import random
+import math
+
+def GenerateX():
+    return random.randint(0, 20)
+
+def GenerateY():
+    return random.randint(0, 20)
+
+def SimulationOfShot(countOfTry,avilibleTries):
+    print("| № Пострілу | Координати Пострілу | Результат |")
+    for i in range(avilibleTries):
+        x = GenerateX()
+        y = GenerateY()
+        formula1 = (x > 0 and x < (2 * R)) and (y > -R and y < 0)
+        formula2 = pow(x, 2) + pow(y - R, 2) < pow(R, 2)
+        countOfTry += 1
+        if (formula1 or formula2):
+            print("|",countOfTry ,"|","(",GenerateX(),",",GenerateY(),")","| Влучив |")
+        else:
+            print("|",countOfTry ,"|","(",GenerateX(),",",GenerateY(),")","| Промах |")
 
 R = 5
-x = random.randint(1, 100)
-y = random.randint(1, 100)
-count = 0
-
-
+avilibleTries = 10
+countOfTry = 0
+SimulationOfShot(countOfTry,avilibleTries)
