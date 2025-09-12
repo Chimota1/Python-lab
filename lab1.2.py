9b248ae82e727dbba8dd8975ce09c1d61983dd25
Anatoliy <avkindrat2006@gmail.com>
2025-09-12 19:06:13 +0300

    End 3 task

--- lab1.2.py
+++ lab1.2.py
@@ -1,9 +1,58 @@
-def Chess():
-    x = 8
-    y = 8
-    desk = [[x][y]]
-    for i in range(x):
-        for j in range(y):
+def WhiteBishop1():
+    x = int(input("Enter x for first bishop1: "))
+    y = int(input("Enter y for first bishop1: "))
+    if (x < 1 or x > 8 or y < 1 or y > 8):
+        print("Error")
+        return None
+    return x, y
 
-Chess()
+def WhiteBishop2():
+    x = int(input("Enter x for first bishop2: "))
+    y = int(input("Enter y for first bishop2: "))
+    if (x < 1 or x > 8 or y < 1 or y > 8):
+        print("Error")
+        return None
+    return x, y
 
+def BlackBishop1():
+    x = int(input("Enter x for first black bishop1: "))
+    y = int(input("Enter y for first black bishop1: "))
+    if (x < 1 or x > 8 or y < 1 or y > 8):
+        print("Error")
+        return None
+    return x, y
+
+def BlackBishop2():
+    x = int(input("Enter x for first black bishop2: "))
+    y = int(input("Enter y for first black bishop2: "))
+    if (x < 1 or x > 8 or y < 1 or y > 8):
+        print("Error")
+        return None
+    return x, y
+
+def Chess(wb1, wb2, bb1, bb2):
+    def can_attack(a, b):
+        return abs(a[0] - b[0]) == abs(a[1] - b[1])
+
+    if can_attack(wb1, bb1) or can_attack(wb1, bb2):
+        print("1 attack")
+    elif can_attack(wb1, wb2):
+        print("2 defend")
+    else:
+        print("3 skip")
+
+    if can_attack(wb2, bb1) or can_attack(wb2, bb2):
+        print("1 attack")
+    elif can_attack(wb2, wb1):
+        print("2 defend")
+    else:
+        print("3 skip")
+
+
+wb1 = WhiteBishop1()
+wb2 = WhiteBishop2()
+bb1 = BlackBishop1()
+bb2 = BlackBishop2()
+
+if wb1 and wb2 and bb1 and bb2:
+    Chess(wb1, wb2, bb1, bb2)
