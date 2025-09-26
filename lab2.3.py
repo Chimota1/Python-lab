66e3939d4a5f1cb504b82c450bf371628b504df5
Anatoliy <avkindrat2006@gmail.com>
2025-09-26 12:04:42 +0300

    End lab2.3

--- lab2.3.py
+++ lab2.3.py
@@ -1,23 +1,38 @@
-import math
 import random
 
-def create_massive(data_market):
-    for i in range(10):
-        row = []
-        for j in range((4)):
-            row.append(random.randint(2000,1000000))
-        data_market.append(row)
+income = []
 
-def show_market(data_market,choice):
-    create_massive(data_market)
-    for i in range(len(data_market)):
-        if i == choice:
-            for j in range(len(data_market[i])):
-                print (data_market[i][j])
+for i in range(10):
+    row = []
+    for j in range(12):
+        value = random.randint(1000, 10000)  # випадковий дохід
+        row.append(value)
+    income.append(row)
 
+print("Дохід магазинів по місяцях:")
+for i in range(10):
+    print("Магазин", i + 1, ":", income[i])
 
-data_market = []
-choice = int(input("Enter the number of market: "))
-show_market(data_market,choice)
+store = int(input("\nВведіть номер магазину (1-10): "))
+store_index = store - 1
 
+quarter = int(input("Введіть номер кварталу (1-4): "))
 
+if quarter == 1:
+    start = 0
+    end = 3
+elif quarter == 2:
+    start = 3
+    end = 6
+elif quarter == 3:
+    start = 6
+    end = 9
+else:
+    start = 9
+    end = 12
+
+quarter_income = 0
+for month in range(start, end):
+    quarter_income += income[store_index][month]
+
+print("\nДохід магазину", store, "за", quarter,"-й квартал:", quarter_income)
