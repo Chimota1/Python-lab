a6f8a7784b96e47c063ea135eac225153edfe805
Anatoliy <avkindrat2006@gmail.com>
2025-10-09 16:02:53 +0300

    change in lab 3.2

--- lab3.2.py
+++ lab3.2.py
@@ -27,44 +27,42 @@ def change_text():
 
 
 root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Regex Editor")
+
 Label(root, text="Enter the sentence: ").grid(row=0, column=0)
 text = Entry(root, width=40)
-text.grid(row=1, column=0)
+text.grid(row=0, column=1)
+btnOutput = tk.Button(root, text="Output", command=outputText)
+btnOutput.grid(row=0, column=2)
 
 lbl_output = Label(root, text="")
-lbl_output.grid(row=2, column=0)
+lbl_output.grid(row=1, column=0, columnspan=3)
 
+button = tk.Button(root, text="Output quantity", command=output_REGEXcount)
+button.grid(row=2, column=0)
 lbl_count_text = Label(root, text="Count:")
-lbl_count_text.grid(row=4, column=0)
+lbl_count_text.grid(row=2, column=1)
 lbl_count = Label(root, text="0")
-lbl_count.grid(row=4, column=1)
+lbl_count.grid(row=2, column=2)
 
+buttonDel = tk.Button(root, text="Remove atrib", command=delete_REGEX)
+buttonDel.grid(row=3, column=0)
+delete_input = Entry(root, width=20)
+delete_input.grid(row=3, column=1)
 lbl_deleted_text = Label(root, text="Deleted Text:")
-lbl_deleted_text.grid(row=5, column=0)
+lbl_deleted_text.grid(row=4, column=0)
 lbl_deleted = Label(root, text="")
-lbl_deleted.grid(row=5, column=1)
+lbl_deleted.grid(row=4, column=1, columnspan=2)
 
+buttonChange = tk.Button(root, text="Change text", command=change_text)
+buttonChange.grid(row=5, column=0)
+lbl_changed_input = Entry(root, width=30)
+lbl_changed_input.grid(row=5, column=1)
 lbl_changed_text = Label(root, text="Changed Text:")
 lbl_changed_text.grid(row=6, column=0)
 lbl_changed = Label(root, text="")
-lbl_changed_input = Entry(root, width=30)
-lbl_changed_input.grid(row=6, column=1)
-lbl_changed.grid(row=6, column=2)
+lbl_changed.grid(row=6, column=1, columnspan=2)
 
-btnOutput = tk.Button(root, text="Output", command=outputText)
-btnOutput.grid(row=1, column=1)
-
-button = tk.Button(root, text="Output quantity", command=output_REGEXcount)
-button.grid(row=3, column=0)
-
-delete_input = Entry(root, width=20)
-delete_input.grid(row=3, column=1)
-buttonDel = tk.Button(root, text="Remove atrib", command=delete_REGEX)
-buttonDel.grid(row=3, column=2)
-
-buttonChange = tk.Button(root, text="Change text", command=change_text)
-buttonChange.grid(row=7, column=0)
-
-root.geometry("600x700")
-root.resizable(False, False)
 root.mainloop()
